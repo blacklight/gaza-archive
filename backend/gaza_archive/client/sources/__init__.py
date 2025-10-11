bbc1e4027a6b0affaec90a685a37754f0334f0fc
Fabio Manganiello <fabio@manganiello.tech>
2025-10-11 16:09:12 +0200

    (Temporarily) disable Molly Shah verification source.
    While pending alignment on the verification framework.

--- backend/gaza_archive/client/sources/__init__.py
+++ backend/gaza_archive/client/sources/__init__.py
@@ -1,7 +1,8 @@
 from .gaza_verified import GazaVerifiedApi
-from .molly_shah import MollyShahApi
+
+# from .molly_shah import MollyShahApi
 
 sources = {
     GazaVerifiedApi,
-    MollyShahApi,
+    # MollyShahApi,
 }
