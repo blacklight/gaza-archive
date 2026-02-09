60f63cebb30fe3ae71f0204a84b4c9b4c8b8e34f
Fabio Manganiello <fabio@manganiello.tech>
2026-02-09 21:21:49 +0100

    fix: When an account changes their campaign URL propagate the change to existing donations too

--- backend/gaza_archive/db/_accounts.py
+++ backend/gaza_archive/db/_accounts.py
@@ -8,7 +8,12 @@ from sqlalchemy import func
 from sqlalchemy.orm import Session
 
 from ..model import Account
-from ._model import Account as DbAccount, Post as DbPost
+from ._model import (
+    Account as DbAccount,
+    Campaign as DbCampaign,
+    CampaignDonation as DbCampaignDonation,
+    Post as DbPost,
+)
 
 log = getLogger(__name__)
 
@@ -104,6 +109,57 @@ class Accounts(ABC):
                 db_account = db_accounts.get(account.url)
                 if db_account and account != db_account.to_model():
                     log.info("Updating account: %s", account.url)
+                    old_campaign_url = str(db_account.campaign_url) if db_account.campaign_url else None
+                    new_campaign_url = account.campaign_url
+                    if (
+                        old_campaign_url
+                        and new_campaign_url
+                        and old_campaign_url != new_campaign_url
+                    ):
+                        old_campaign = (
+                            session.query(DbCampaign)
+                            .filter(DbCampaign.url == old_campaign_url)  # type: ignore
+                            .first()
+                        )
+                        new_campaign = (
+                            session.query(DbCampaign)
+                            .filter(DbCampaign.url == new_campaign_url)  # type: ignore
+                            .first()
+                        )
+
+                        if old_campaign:
+                            if not new_campaign:
+                                new_campaign = DbCampaign(
+                                    url=new_campaign_url,
+                                    donations_cursor=old_campaign.donations_cursor,
+                                )
+                                session.add(new_campaign)
+                                session.flush()
+                            elif (
+                                not new_campaign.donations_cursor
+                                and old_campaign.donations_cursor
+                            ):
+                                new_campaign.donations_cursor = old_campaign.donations_cursor
+                                session.add(new_campaign)
+
+                            session.query(DbCampaignDonation).filter(
+                                DbCampaignDonation.campaign_url == old_campaign_url
+                            ).update(
+                                {DbCampaignDonation.campaign_url: new_campaign_url},
+                                synchronize_session=False,
+                            )
+                        else:
+                            if not new_campaign:
+                                session.add(DbCampaign(url=new_campaign_url))
+                    elif new_campaign_url and not old_campaign_url:
+                        existing_campaign = (
+                            session.query(DbCampaign)
+                            .filter(DbCampaign.url == new_campaign_url)  # type: ignore
+                            .first()
+                        )
+                        if not existing_campaign:
+                            session.add(DbCampaign(url=new_campaign_url))
+
                     db_account.update_from_model(account)
                     session.merge(db_account)
                 elif not db_account:
